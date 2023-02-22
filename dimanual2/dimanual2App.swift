956f9edc5a885812860cb85353ae8360780bcbe7
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-02-22 17:35:26 -0300

    Initial Commit

---
+++ dimanual2/dimanual2App.swift
@@ -0,0 +1,17 @@
+//
+//  dimanual2App.swift
+//  dimanual2
+//
+//  Created by Jonn Alves on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct dimanual2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
